5a344bead57773ef38582187f94725389c9cb28a
Mohammed Altheeb <altheebinfo@gmail.com>
2025-02-03 00:39:19 +0300

    feat: calculate equal split

--- batware/batware/doctype/expense/test_expense.py
+++ batware/batware/doctype/expense/test_expense.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2025, Mohammed Al-Theeb and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestExpense(FrappeTestCase):
-	pass
+	def test_equal_split_calculation(self):
+		#TODO: also create test users before
+		test_expense = frappe.get_doc({
+			"doctype": "Expense",
+			"description": "test expense",
+			"paid_by": "friend1@gmail.com",
+			"amount": 200,
+			"splits": [
+				{
+					"user": "friend1@gmail.com"
+				},
+				{
+					"user": "friend2@gmail.com"
+				}
+			]
+		}).insert()
+
+		self.assertEqual(test_expense.splits[0].amount, 100)
+		self.assertEqual(test_expense.splits[1].amount, 100)
+
+		
